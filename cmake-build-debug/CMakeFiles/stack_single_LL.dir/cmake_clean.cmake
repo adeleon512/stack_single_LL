c56718d7d8abea7070a8db9ab27d67d521d8e05f
Adam <acd21tx@gmail.com>
2017-02-26 15:24:29 -0600

    initial commit for stack_single_LL

---
+++ cmake-build-debug/CMakeFiles/stack_single_LL.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/stack_single_LL.dir/main.cpp.obj"
+  "stack_single_LL.pdb"
+  "stack_single_LL.exe"
+  "stack_single_LL.exe.manifest"
+  "libstack_single_LL.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/stack_single_LL.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
